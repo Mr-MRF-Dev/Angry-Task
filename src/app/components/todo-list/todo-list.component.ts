e069e7434c20adb5e38e3af610e0eb07a11831cd
Mr MRF <79112136+Mr-MRF-Dev@users.noreply.github.com>
2024-09-13 03:11:37 +0330

    faat: create task model :wolf:

--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
 import { CardModule } from 'primeng/card';
+import { Task } from '../../models/task';
+import { NgForOf } from '@angular/common';
 
 @Component({
   selector: 'app-todo-list',
   standalone: true,
-  imports: [CardModule],
+  imports: [NgForOf, CardModule],
   templateUrl: './todo-list.component.html',
   styleUrl: './todo-list.component.css',
 })
-export class TodoListComponent {}
+export class TodoListComponent {
+  tasks: Task[] = [
+    { id: 1, title: 'Buy milk', completed: false },
+    { id: 2, title: 'Buy bread', completed: true },
+    { id: 3, title: 'Buy cheese', completed: false },
+    { id: 4, title: 'Buy butter', completed: false },
+    { id: 5, title: 'Buy eggs', completed: true },
+    { id: 6, title: 'Buy jam', completed: true },
+    { id: 7, title: 'Buy coffee', completed: false },
+  ];
+}
