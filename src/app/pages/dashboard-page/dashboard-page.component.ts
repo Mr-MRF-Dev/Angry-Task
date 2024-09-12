14e25682e521dedde0cd476cadec0ac415a0a737
Mr MRF <79112136+Mr-MRF-Dev@users.noreply.github.com>
2024-09-13 02:54:07 +0330

    refactor: use todo list in dashboard page

--- src/app/pages/dashboard-page/dashboard-page.component.ts
+++ src/app/pages/dashboard-page/dashboard-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { DashboardLayoutComponent } from '../../layouts/dashboard-layout/dashboard-layout.component';
+import { TodoListComponent } from '../../components/todo-list/todo-list.component';
 
 @Component({
   selector: 'app-dashboard-page',
   standalone: true,
-  imports: [DashboardLayoutComponent],
+  imports: [DashboardLayoutComponent, TodoListComponent],
   templateUrl: './dashboard-page.component.html',
   styleUrl: './dashboard-page.component.css',
 })
