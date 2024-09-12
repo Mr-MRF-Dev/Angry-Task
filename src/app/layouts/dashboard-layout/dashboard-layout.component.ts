6c75d4e6ad1e3d92cfa613c785fe19bc6edd2696
Mr MRF <79112136+Mr-MRF-Dev@users.noreply.github.com>
2024-09-12 18:29:24 +0330

    feat: create top bar menu

--- src/app/layouts/dashboard-layout/dashboard-layout.component.ts
+++ src/app/layouts/dashboard-layout/dashboard-layout.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { MenubarModule } from 'primeng/menubar';
+import { ButtonModule } from 'primeng/button';
+import { AvatarModule } from 'primeng/avatar';
 
 @Component({
   selector: 'app-dashboard-layout',
   standalone: true,
-  imports: [],
+  imports: [AvatarModule, MenubarModule, ButtonModule],
+
   templateUrl: './dashboard-layout.component.html',
   styleUrl: './dashboard-layout.component.css',
 })
-export class DashboardLayoutComponent {}
+export class DashboardLayoutComponent {
+  changeThemeMode() {}
+
+  changePalette() {}
+}
