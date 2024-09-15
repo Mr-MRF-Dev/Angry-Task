4fcee97ec5e22593e84f3ca575e9cf58736a9c3c
Mr MRF <79112136+Mr-MRF-Dev@users.noreply.github.com>
2024-09-15 04:03:33 +0330

    revert: remove delete task dialog

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -4,21 +4,12 @@ import { CheckboxModule } from 'primeng/checkbox';
 import { FormsModule } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
 import { MenuModule } from 'primeng/menu';
-import { ConfirmationService, MenuItem } from 'primeng/api';
+import { MenuItem } from 'primeng/api';
 import { PanelModule } from 'primeng/panel';
-import { ConfirmDialogModule } from 'primeng/confirmdialog';
 @Component({
   selector: 'app-task-item',
   standalone: true,
-  imports: [
-    ConfirmDialogModule,
-    PanelModule,
-    FormsModule,
-    ButtonModule,
-    CheckboxModule,
-    MenuModule,
-  ],
-  providers: [ConfirmationService],
+  imports: [PanelModule, FormsModule, ButtonModule, CheckboxModule, MenuModule],
   templateUrl: './task-item.component.html',
   styleUrl: './task-item.component.css',
 })
@@ -28,8 +19,6 @@ export class TaskItemComponent implements OnInit {
 
   items: MenuItem[] | undefined;
 
-  constructor(private confirmationService: ConfirmationService) {}
-
   ngOnInit() {
     this.items = [
       { label: 'Edit', icon: 'pi pi-pencil', command: () => this.editTask() },
@@ -37,31 +26,11 @@ export class TaskItemComponent implements OnInit {
       {
         label: 'Delete',
         icon: 'pi pi-trash',
-        command: () => this.deleteTaskConfirm(),
+        command: () => {
+          this.deleteTask.emit(this.task.id);
+        },
       },
     ];
-  }
-
-  protected deleteTaskConfirm() {
-    this.confirmationService.confirm({
-      message: 'Do you want to delete this task?',
-      header: 'Danger Zone',
-      icon: 'pi pi-info-circle',
-      rejectLabel: 'Cancel',
-      rejectButtonProps: {
-        label: 'Cancel',
-        severity: 'secondary',
-        outlined: true,
-      },
-      acceptButtonProps: {
-        label: 'Delete',
-        severity: 'danger',
-      },
-
-      accept: () => {
-        this.deleteTask.emit(this.task.id);
-      },
-    });
   }
 
   editTask() {
