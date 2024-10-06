b1c15954c3cdfa5a21c658e994dc10411cd7ef4b
Mr MRF <79112136+Mr-MRF-Dev@users.noreply.github.com>
2024-10-06 23:41:32 +0330

    chore: remove message service

--- src/app/components/task-list/task-list.component.ts
+++ src/app/components/task-list/task-list.component.ts
@@ -1,9 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { CardModule } from 'primeng/card';
 import { Task } from '../../models/task';
-import { ToastModule } from 'primeng/toast';
 import { TaskItemComponent } from '../task-item/task-item.component';
-import { MessageService } from 'primeng/api';
 import { ButtonModule } from 'primeng/button';
 import { AddTaskDialogComponent } from '../add-task-dialog/add-task-dialog.component';
 import { TaskList } from '../../models/task_list';
@@ -13,43 +11,24 @@ import { TaskListHandlerService } from '../../services/task-list-handler.service
   selector: 'app-task-list',
   standalone: true,
   imports: [
-    ToastModule,
     CardModule,
     ButtonModule,
     TaskItemComponent,
     AddTaskDialogComponent,
   ],
-  providers: [MessageService],
   templateUrl: './task-list.component.html',
   styleUrl: './task-list.component.css',
 })
 export class TaskListComponent {
-  constructor(
-    private messageService: MessageService,
-    private taskListHandlerService: TaskListHandlerService,
-  ) {}
+  constructor(private taskListHandlerService: TaskListHandlerService) {}
 
   @Input() self!: TaskList;
 
   deleteTaskHandler(id: Task['id']) {
     this.taskListHandlerService.deleteTask(this.self.id, id);
-    this.self.tasks = this.self.tasks.filter((task) => task.id !== id);
-    // this.messageService.add({
-    //   severity: 'info',
-    //   summary: 'Task Deleted',
-    //   detail: `task '${targetTask?.title}' has been deleted`,
-    //   life: 3000,
-    // });
   }
 
   newTaskHandler(task: Task) {
     this.taskListHandlerService.createTask(this.self.id, task);
-
-    this.messageService.add({
-      severity: 'success',
-      summary: 'Task Added',
-      detail: `task '${task.title}' has been added`,
-      life: 3000,
-    });
   }
 }
