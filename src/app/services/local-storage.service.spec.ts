65ec35f513e4d80b91b10a2fc242ead7a14e473f
Mr MRF <79112136+Mr-MRF-Dev@users.noreply.github.com>
2024-10-10 00:03:03 +0330

    test: localStorageService

--- src/app/services/local-storage.service.spec.ts
+++ src/app/services/local-storage.service.spec.ts
@@ -10,7 +10,44 @@ describe('LocalStorageService', () => {
     service = TestBed.inject(LocalStorageService);
   });
 
-  it('should be created', () => {
+  afterAll(() => {
+    localStorage.clear();
+  });
+
+  it('SHOULD be created WHEN the service is instantiated', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('SHOULD save the Array in the local storage WHEN saveArray is called', () => {
+    const key = 'key';
+    const value = [{ name: 'qwqw' }, { name: 'wqwq' }];
+
+    service.saveArray(key, value);
+
+    expect(localStorage.getItem(key)).toBe(JSON.stringify(value));
+  });
+
+  it("SHOULD return an empty array WHEN loadArray is called and the key doesn't exist", () => {
+    const key = 'key2';
+
+    expect(service.loadArray(key)).toEqual([]);
+  });
+
+  it('SHOULD return the array from the local storage WHEN loadArray is called and the key exists', () => {
+    const key = 'key3';
+    const value = [{ name: 'qwqw' }, { name: 'wqwq' }];
+
+    localStorage.setItem(key, JSON.stringify(value));
+    expect(service.loadArray(key)).toEqual(value);
+  });
+
+  it('SHOULD remove the item from the local storage WHEN remove is called', () => {
+    const key = 'key4';
+    const value = [{ name: 'qwqw' }, { name: 'wqwq' }];
+
+    localStorage.setItem(key, JSON.stringify(value));
+    service.remove(key);
+
+    expect(localStorage.getItem(key)).toBeNull();
   });
 });
