8376ebed623eebed1454d544b8cd3946d91e804b
Mr MRF <79112136+Mr-MRF-Dev@users.noreply.github.com>
2024-10-09 23:08:48 +0330

    test: Add tests for EditTaskHandlerService

--- src/app/services/edit-task-handler.service.spec.ts
+++ src/app/services/edit-task-handler.service.spec.ts
@@ -1,6 +1,8 @@
 import { TestBed } from '@angular/core/testing';
 
 import { EditTaskHandlerService } from './edit-task-handler.service';
+import { TaskList } from '../models/task_list';
+import { Task } from '../models/task';
 
 describe('EditTaskHandlerService', () => {
   let service: EditTaskHandlerService;
@@ -12,5 +14,22 @@ describe('EditTaskHandlerService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should return an observable', () => {
+    expect(service.getObservable()).toBeTruthy();
+  });
+
+  it('should emit task edit request', (done) => {
+    const taskListId: TaskList['id'] = 123;
+    const task: Task = { id: 234, title: 'Test Task', completed: false };
+
+    service.getObservable().subscribe(([emittedTaskListId, emittedTask]) => {
+      expect(emittedTaskListId).toBe(taskListId);
+      expect(emittedTask).toEqual(task);
+      done();
+    });
+
+    service.requestEditTask(taskListId, task);
   });
 });
