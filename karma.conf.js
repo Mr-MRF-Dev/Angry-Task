e9632adce8ad7225ae940ba1eb6630cf95af8715
Mr MRF <79112136+Mr-MRF-Dev@users.noreply.github.com>
2024-10-11 00:29:10 +0330

    feat: add karma config

---
+++ karma.conf.js
@@ -0,0 +1,45 @@
+// Karma configuration
+module.exports = function (config) {
+  config.set({
+    basePath: "",
+    frameworks: ["jasmine", "@angular-devkit/build-angular"],
+    reporters: ["progress"],
+
+    plugins: [
+      require("karma-jasmine"),
+      require("karma-chrome-launcher"),
+      require("karma-jasmine-html-reporter"),
+      require("karma-coverage"),
+      require("@angular-devkit/build-angular/plugins/karma"),
+    ],
+    client: {
+      jasmine: {},
+      clearContext: false,
+    },
+
+    jasmineHtmlReporter: {
+      suppressAll: true,
+    },
+
+    coverageReporter: {
+      dir: require("path").join(__dirname, "./coverage/angry-task"),
+      subdir: ".",
+      reporters: [
+        { type: "html" },
+        { type: "text-summary" },
+        { type: "lcovonly" },
+      ],
+      check: {
+        global: {
+          statements: 80,
+          branches: 80,
+          functions: 80,
+          lines: 80,
+        },
+      },
+    },
+    reporters: ["progress", "kjhtml"],
+    browsers: ["Chrome"],
+    restartOnFileChange: true,
+  });
+};
