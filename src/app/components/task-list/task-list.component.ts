b687d967a06470525091a66900227dc5f0ade683
Mr MRF <79112136+Mr-MRF-Dev@users.noreply.github.com>
2024-10-16 20:56:40 +0330

    feat: add task list delete function and btn

--- src/app/components/task-list/task-list.component.ts
+++ src/app/components/task-list/task-list.component.ts
@@ -4,6 +4,7 @@ import { TaskItemComponent } from '../task-item/task-item.component';
 import { ButtonModule } from 'primeng/button';
 import { TaskList } from '../../models/task_list';
 import { NewTaskHandlerService } from '../../services/new-task-handler.service';
+import { TaskListHandlerService } from '../../services/task-list-handler.service';
 
 @Component({
   selector: 'app-task-list',
@@ -14,10 +15,14 @@ import { NewTaskHandlerService } from '../../services/new-task-handler.service';
 })
 export class TaskListComponent {
   @Input() self!: TaskList;
-
+  private taskListHandlerService = inject(TaskListHandlerService);
   private newTaskHandlerService = inject(NewTaskHandlerService);
 
   createNewTask() {
     this.newTaskHandlerService.requestNewTask(this.self.id);
+  }
+
+  deleteTaskList() {
+    this.taskListHandlerService.deleteTaskList(this.self.id);
   }
 }
